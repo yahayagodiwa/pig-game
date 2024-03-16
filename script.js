c189b9f59b3beaa2cd3bcbcd39f0c92b51ee1b0b
yahayagodiwa <126706373+yahayagodiwa@users.noreply.github.com>
2024-03-16 03:40:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+'use strict';
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const diceRoll = document.querySelector('.btn--roll');
+const diceNew = document.querySelector('.btn--new');
+const diceHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  current0.textContent = 0;
+  current1.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+///initialization function
+init();
+
+const switchplayer = function () {
+  const currentPlayer = document.querySelector(`#current--${activePlayer}`);
+  currentPlayer.textContent = currentScore;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  //switch active player design
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+//adding eventlistener to roll  button
+
+diceRoll.addEventListener('click', () => {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+      currentScore += dice;
+
+      document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+    } else {
+      switchplayer();
+    }
+  }
+});
+
+// hold the scores///////////////////////////
+
+diceHold.addEventListener('click', () => {
+  if (playing) {
+   
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      diceEl.classList.add('hidden');
+    } else {
+      switchplayer();
+    }
+  }
+});
+
+////////////////// rest the game //////////////
+
+diceNew.addEventListener('click', init);
